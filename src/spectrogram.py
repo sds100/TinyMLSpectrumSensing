40e55fdee38b2b231304c1375f94afcd4df03a62
sds100 <developer.sds100@gmail.com>
2024-05-12 12:43:54 +0200

    move spectrum painting code to separate files

---
+++ src/spectrogram.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from typing import Iterable, List
+
+import numpy as np
+import numpy.typing as npt
+from scipy import signal
+from skimage.transform import downscale_local_mean
+
+
+@dataclass
+class Spectrogram:
+    """
+    This stores the information required to interpret a spectrogram.
+
+    f are the frequencies
+    t are an array of time intervals
+    values is a 2D numpy array for the magnitudes for each frequency f at time t.
+    """
+    f: [int]
+    t: [int]
+    values: npt.NDArray[float]
+
+
+def move_front_half_to_end(array: npt.NDArray) -> npt.NDArray:
+    """
+    Move the front half of an array to the end
+    """
+    n = len(array)
+    return np.concatenate((array[n // 2:], array[:n // 2]))
+
+
+def create_spectrogram(x: Iterable, fs: int) -> Spectrogram:
+    """
+    Create a spectrogram of a signal.
+    :param x: The signal
+    :param fs: The sample rate in Hz.
+    :return: A Spectrogram instance that contains the frequencies, time and values.
+    """
+
+    # Calculate Short Time Fourier Transform of the signal.
+    # return_one_sided is needed because the data is complex
+    f, t, Zxx = signal.stft(x=x, fs=fs, return_onesided=False)
+
+    # A spectrogram is the absolute value of the STFT and then squared.
+    Zxx_abs = (np.abs(Zxx) ** 2)
+
+    # For some reason our spectrogram has the negative frequencies in
+    # the *last* half of the data so this method moves to the front
+    # This can help with plotting because to use 'nearest' shading requires the
+    # data to be monotonically increasing/decreasing in value.
+    return Spectrogram(move_front_half_to_end(f), t, move_front_half_to_end(Zxx_abs))
+
+
+def split_spectrogram(spectrogram: Spectrogram, duration: int) -> List[Spectrogram]:
+    """
+    Split up a spectrogram along the time axis into a bunch of smaller spectrograms.
+    :param spectrogram: The spectrogram to split
+    :param duration: The length of each sub-spectrogram
+    :return: A list of sub spectrograms.
+    """
+    sub_spectrograms: [Spectrogram] = []
+
+    start_index: int = 0
+    end_index: int = duration
+
+    while end_index < len(spectrogram.t):
+        values_slice = [time_values[start_index:end_index] for time_values in spectrogram.values]
+
+        sub_spectrograms.append(
+            Spectrogram(spectrogram.f,
+                        spectrogram.t[start_index:end_index],
+                        np.array(values_slice)))
+
+        start_index += duration
+        end_index += duration
+
+    return sub_spectrograms
---
+++ src/spectrum_painting.py
@@ -0,0 +1,92 @@
+import numpy as np
+import numpy.typing as npt
+from skimage.transform import downscale_local_mean
+
+from src.spectrogram import Spectrogram
+
+
+def take_frequencies(spec: Spectrogram, start: int, end: int) -> Spectrogram:
+    # for spectrum painting to work best, the WiFi signal must
+    # fill the spectrogram so only take the frequencies for the
+    # WiFi signal
+    spec.f = spec.f[start:end]
+    spec.values = spec.values[start:end]
+    return spec
+
+
+def downsample(spectrogram: npt.NDArray[float], resolution: int) -> npt.NDArray:
+    """
+    Downsample a spectrogram to a target N x N resolution.
+
+    :param spectrogram: A 2D array of a spectrogram. 
+    :arg resolution: The target height/width of the image in pixels.
+    :return: The downsampled spectrogram.
+    """
+
+    (height, width) = spectrogram.shape
+    time_factor = width // resolution
+    freq_factor = height // resolution
+    downsampled_spec_values = downscale_local_mean(spectrogram, (freq_factor, time_factor))[:resolution, :resolution]
+
+    assert downsampled_spec_values.shape == (resolution, resolution)
+    return downsampled_spec_values
+
+
+def augment(spectrogram: npt.NDArray[float], k: int, l: int, d: int) -> npt.NDArray[float]:
+    """
+    Augment the Bluetooth and Zigbee signals by stretching them.
+
+    :param spectrogram: An M x N array of signal magnitude values.
+    :param k: The number of maximum values
+    :param l: The sliding window size
+    :param d: The step size
+    """
+
+    # copy the spectrogram so modifying the values in-place
+    # does not change the argument.
+    spectrogram_copy = np.copy(spectrogram).T
+    (M, N) = spectrogram_copy.shape
+
+    augmented_width = (N - l) // d + 1
+    augmented_spectrogram: np.ndarray = np.zeros(shape=(M, augmented_width), dtype=float)
+
+    m = np.mean(spectrogram_copy)
+
+    for i in range(1, M):
+        j_augmented = 0
+        j = 0
+        while j <= N - l:
+            # For the current row i, get a window of size L.
+            window = spectrogram_copy[i][j:(j + l)]
+            # Get the top K elements by sorting and taking last K elements.
+            top_k = np.sort(window)[len(window) - k:]
+            mean_top_k = np.mean(top_k)
+            spectrogram_copy[i][j] = mean_top_k
+            augmented_spectrogram[i][j_augmented] = spectrogram_copy[i][j] - m
+
+            j_augmented += 1
+            j += d
+
+    return augmented_spectrogram.clip(0, 1)
+
+
+def paint_spectrogram(original: npt.NDArray, augmented: npt.NDArray) -> npt.NDArray:
+    painted_spectrogram = np.zeros(shape=augmented.shape)
+    mean_original_rows = np.mean(original, axis=0)
+
+    for row in range(len(augmented)):
+        painted_spectrogram[row] = augmented[row] - mean_original_rows[row]
+
+    # clip the values so they are all positive
+    return painted_spectrogram.clip(min=0)
+
+
+def digitize_spectrogram(spectrogram: npt.NDArray[float], color_depth: int) -> npt.NDArray[int]:
+    max_value: float = spectrogram.max()
+    scale: float = color_depth / max_value
+    spectrogram = spectrogram.clip(0)
+    scaled_spectrogram = spectrogram * scale
+
+    bins = np.arange(color_depth, dtype=int)
+    bin_indices = np.digitize(scaled_spectrogram, bins)
+    return bins[bin_indices - 1]
