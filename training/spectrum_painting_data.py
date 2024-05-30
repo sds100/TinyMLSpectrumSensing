c4a034da8ed796ed8edb5d6f8e678fb42df9df04
sds100 <seth.developer@proton.me>
2024-05-30 18:18:51 +0200

    convert matlab files to numpy array files to save a lot of time when creating spectrograms

--- training/spectrum_painting_data.py
+++ training/spectrum_painting_data.py
@@ -7,6 +7,32 @@ import scipy.io as sio
 from training.spectrogram import Spectrogram, create_spectrogram
 
 
+def load_data_from_matlab(file: str) -> npt.NDArray[np.complex128]:
+    """
+    Load the list of complex numbers from Matlab files.
+    """
+    # each complex number is in its own row and so is put in
+    # its own array. 'squeeze' flattens the array.
+    return sio.loadmat(file)["WaveformOut"].squeeze()
+
+
+def convert_matlab_to_numpy(matlab_dir: str,
+                            numpy_dir: str,
+                            classes: List[str],
+                            snr_list: List[int]):
+    """
+    :param matlab_dir: The directory where the data files are stored. The files must be named with
+                    the following pattern: SNR_{snr}_{class}.mat where snr is the signal-to-noise ratio
+                    and class is which types of signals are present - e.g Z for Zigbee, ZW for Zigbee and WiFi.
+                    The .mat files should contain one object called "WaveformOut".
+    """
+    for c in classes:
+        for snr in snr_list:
+            data = load_data_from_matlab(f"{matlab_dir}/SNR{snr}_{c}.mat")
+
+            np.save(f"{numpy_dir}/SNR{snr}_{c}.npy", data)
+
+
 def load_spectrograms(data_dir: str,
                       classes: List[str],
                       snr_list: List[int],
@@ -16,9 +42,9 @@ def load_spectrograms(data_dir: str,
     Read the time-domain data and convert it to spectrograms.
 
     :param data_dir: The directory where the data files are stored. The files must be named with
-                    the following pattern: SNR_{snr}_{class}.mat where snr is the signal-to-noise ratio
+                    the following pattern: SNR_{snr}_{class}.npy where snr is the signal-to-noise ratio
                     and class is which types of signals are present - e.g Z for Zigbee, ZW for Zigbee and WiFi.
-                    The .mat files should contain one object called "WaveformOut".
+                    The .npy files should contain an array of np.complex128 numbers.
     :param classes: The classes to load.
     :param snr_list: The signal-to-noise ratios to load.
     :param sample_rate: The sample rate of the data.
@@ -27,23 +53,15 @@ def load_spectrograms(data_dir: str,
     :return: A dictionary that maps each class to a list of spectrograms with
              different signal-to-noise ratios.
     """
-
-    def load_data_from_matlab(file: str) -> npt.NDArray[np.complex128]:
-        """
-        Load the list of complex numbers from Matlab files.
-        """
-        # each complex number is in its own row and so is put in
-        # its own array. 'squeeze' flattens the array.
-        return sio.loadmat(file)["WaveformOut"].squeeze()[:count]
-
     spectrograms: Dict[str, List[Spectrogram]] = {}
 
     for c in classes:
         spectrograms[c] = []
 
         for snr in snr_list:
-            data = load_data_from_matlab(f"{data_dir}/SNR{snr}_{c}.mat")
-            spectrogram = create_spectrogram(data, sample_rate, snr)
+            data = np.load(f"{data_dir}/SNR{snr}_{c}.npy")
+
+            spectrogram = create_spectrogram(data[:count], sample_rate, snr)
             spectrograms[c].append(spectrogram)
 
     return spectrograms
