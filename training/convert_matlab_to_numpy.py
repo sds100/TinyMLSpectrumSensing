ac03c60c2548032fe78805119926ea4482c60767
sds100 <seth.developer@proton.me>
2024-06-01 14:40:15 +0200

    create file to convert matlab to numpy file

---
+++ training/convert_matlab_to_numpy.py
@@ -0,0 +1,6 @@
+import spectrum_painting_data as sp_data
+
+sp_data.convert_matlab_to_numpy(matlab_dir="data/matlab-test",
+                                numpy_dir="data/numpy-test",
+                                classes=["Z", "B", "W", "BW", "ZB", "ZW", "ZBW"],
+                                snr_list=[0, 10, 20, 30])
