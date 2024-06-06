a7d5688b413254a9ba7888282836553eeadb74fb
sds100 <seth.developer@proton.me>
2024-06-06 11:46:12 +0200

    use single precision complex numbers when converting to numpy

--- training/convert_matlab_to_numpy.py
+++ training/convert_matlab_to_numpy.py
@@ -1,6 +1,6 @@
 import spectrum_painting_data as sp_data
 
-sp_data.convert_matlab_to_numpy(matlab_dir="data/matlab-random-2",
-                                numpy_dir="data/numpy-random-2",
+sp_data.convert_matlab_to_numpy(matlab_dir="data/matlab-2",
+                                numpy_dir="data/numpy-2",
                                 classes=["Z", "B", "W", "BW", "ZB", "ZW", "ZBW"],
                                 snr_list=[30])
